c39913d4f51a7002ede71fb2fb67cd2cbb30b83a
Leo-768 <leo768leo768@gmail.com>
2020-08-12 20:43:19 +0800

    .

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const Keyv = require('keyv');
 const client = new Discord.Client();
 const log = new Discord.WebhookClient(process.env.WID,process.env.WT);
-const settings = new Keyv('sqlite://isbn/afkt/fdvxcc.sqlite');
+const settings = new Keyv('mongodb://leo768:1234@localhost:27017/bot');
 
 keyv.on('error', err => console.log('Connection Error', err));
 
